152536347e5827e574c01606cfd098021a1f363c
KangHyunWook <kangtaepoong@naver.com>
2021-04-04 15:04:24 +0900

    Update union.c

--- union.c
+++ union.c
@@ -5,21 +5,42 @@ Description: union example
 
 #include <stdio.h>
 
-union fruit{
+#define BANANA 7
+#define APPLE 5
+#define ORANGE 130
+
+struct StructFruit {
+	int banana;
+	char apple;
+	int orange;
+};
+
+union UnionFruit { 
 	int banana;
 	char apple;
 	int orange;
 };
 
 main() {
-	union fruit f;
-	f.banana=7;
-	f.apple=5;
-	f.orange=130;
+	struct StructFruit structFruit;
+	union UnionFruit unionFruit;
 	
-	printf("banana: %d\n", f.banana);
-	printf("apple: %d\n", f.apple);
-	printf("orange: %d\n", f.orange);
+	printf("==========Struct Fruit==========\n");
+	structFruit.banana=7; 
+	structFruit.apple=3; 
+	structFruit.orange=130; 
+	printf("banana: %d\n", structFruit.banana);		
+	printf("apple: %d\n", structFruit.apple);		
+	printf("orange: %d\n", structFruit.orange);
+	
+	printf("==========Union Fruit==========\n");	
+	unionFruit.banana=7;
+	unionFruit.apple=3; 
+	unionFruit.orange=130; 
+
+	printf("banana: %d\n", unionFruit.banana);		
+	printf("apple: %d\n", unionFruit.apple);		
+	printf("orange: %d\n", unionFruit.orange);		
 }
 
 
